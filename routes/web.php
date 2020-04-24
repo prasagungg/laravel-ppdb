da5c35e79eb6ac0316ecc51947000a5b8c9f23e5
agungp <55535250+agungprasetio18@users.noreply.github.com>
2020-04-24 15:09:54 +0700

    tamplating

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/admin', 'AdminController@index');
+
